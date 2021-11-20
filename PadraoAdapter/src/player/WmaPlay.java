17bea86ce8df6bb55ddaadf44addb168ede53399
rodrigobock <rodrigocabralbock@gmail.com>
2021-11-19 22:42:58 -0300

    Padrão Adapter

---
+++ PadraoAdapter/src/player/WmaPlay.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package player;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class WmaPlay {
+
+    public void setFile(String file) {
+
+    }
+
+    public void open() {
+
+    }
+
+    public void setLocation(int position) {
+
+    }
+
+    public void play() {
+
+    }
+
+    public void stop() {
+
+    }
+}
